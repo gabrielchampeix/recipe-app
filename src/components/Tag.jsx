d6f0453027683841baac7f226c73b063a2e8c147
Gabriel Champeix <gabriel.champeix@gmail.com>
2026-04-05 20:21:29 +0200

    Tags for DB are now plugged in and generate the list of Tags in the search

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,6 +1,9 @@
-export default function Tag({ label = "tag", canBeSelected = false, canBeDeleted = false, tagDeleteHandle }) {
+import { useState } from "react"
+
+export default function Tag({ label = "tag", canBeSelected = false, canBeDeleted = false, tagDeleteHandle, clickHandle }) {
+    const [selected, setSelected] = useState(false)
     return (
-        <li style={{ padding: "4px", border: "1px solid grey", listStyle: "none", display: "inline" }}>
+        <li onClick={() => setSelected(!selected)} style={{ padding: "4px", border: selected ? "1px solid black" : "1px solid lightgray", listStyle: "none", display: "inline" }}>
             {label}
             {canBeDeleted ? <button type="button" onClick={() => tagDeleteHandle(label)}>X</button> : ""}
         </li>
